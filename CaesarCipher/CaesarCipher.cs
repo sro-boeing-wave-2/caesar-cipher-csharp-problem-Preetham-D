c9b51fad65156c1bee15bc1bc02bc89d1c974204
Preetham D <preethamdbangera@gmail.com>
2018-07-19 14:57:56 +0530

    Using ascii

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,33 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            
+            int[] asciiArray = new int[text.Length];
+            Char[] textArray = text.ToCharArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                asciiArray[i] = textArray[i];
+                //Console.Write(asciiArray[i]);
+                if ((asciiArray[i] >= (int)'A' && asciiArray[i] <= (int)'Z'))
+                {
+                    asciiArray[i] += shiftKey;
+                    if (asciiArray[i] > (int)'Z') asciiArray[i] -= 26;
+                }
+                if (asciiArray[i] >= (int)'a' && asciiArray[i] <= (int)'z')
+                {
+                    asciiArray[i] += shiftKey;
+                    if (asciiArray[i] > (int)'z') asciiArray[i] -= 26;
+                }
+            }
+            char[] ceaserstring = new char[text.Length];
+            for(int i = 0; i < text.Length; i++)
+            {
+               ceaserstring[i] = Convert.ToChar(asciiArray[i]);
+            }
+            string answer = new string(ceaserstring);
+            return answer;
+            //return ceaserstring.ToString();
+
         }
     }
 }
